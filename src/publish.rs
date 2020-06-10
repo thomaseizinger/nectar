f1cc4c601023b6a69c101e8106512ffe75c47e80
Daniel Karzel <daniel@comit.network>
2020-06-10 20:06:52 +1000

    WIP - trying to make things more understandable and adding TODOs for clarification

--- src/publish.rs
+++ src/publish.rs
@@ -5,6 +5,7 @@ pub trait LockedFunds {
 }
 
 pub trait Balance {
+    // TODO: Is it the nominal balance? (balance in BTC or Satoshi)
     fn balance(&self) -> u64;
 }
 
@@ -12,7 +13,11 @@ pub trait Fees {
     fn fees(&self) -> u64;
 }
 
+// TODO: I would represent this with floats in here and calculate everything using BTC and DAI (because the rate is represented like that too, it will be way easier to understand)
+/// order amounts in smallest units (i.e. satoshi/wei)
 struct Order {
+    // TODO: What are these amounts? Is each amount supposed to be in the respective asset's smallest unit?
+
     pub sell_amount: u64,
     pub buy_amount: u64,
 }
@@ -35,8 +40,22 @@ impl Spread {
     }
 }
 
-/// mid_market_rate is buy/sell: 1 Buy => mid_market_rate Sell: = sell/buy
-/// spread_pc: percent value to be added to the buy amount
+/// The maker creates an order that defines how much he wants to buy for the amount he is selling.
+/// order's buy amount = what the maker wants from a taker
+/// order's sell amount = what the maker is offering to a taker
+///
+/// The function expects the mid_market_rate according to the minimal unit of the sell asset.
+///
+/// mid_market_rate is set as 1 sell => x buy, where x is the mid_market_rate
+///
+/// BTC-DAI: When selling 1 BTC we should buy 9000 DAI, mid_market_rate is 1:9000
+/// Given BTC:DAI and the rate of 1:9000
+///     selling 1.0 BTC with spread_pc of 3% => buy 9270 DAI
+///     selling 0.5 BTC with spread_pc of 3% => buy 4635 DAI
+/// Given DAI:BTC and a rate of 1:0.0001
+///     selling 10000 DAI with spread_pc of 3% => buy 1.03 BTC
+///     selling 1000 DAI with spread_pc of 3% => buy 0.103 DAI
+///
 #[allow(clippy::cast_precision_loss)] // It's ok because it just means we are applying slightly more than the given spread
 fn new_order<W, B>(
     sell_wallet: W,
@@ -52,13 +71,14 @@ where
     let sell_amount =
         min(sell_wallet.balance() - book.locked_funds(), max_sell_amount) - sell_wallet.fees();
 
-    let rate = mid_market_rate / (1.0 + spread.as_f64());
-
-    let buy_amount = sell_amount as f64 / rate;
+    let rate = mid_market_rate * (1.0 + spread.as_f64());
+    // TODO: The ceil() only makes sense if we really use the smallest units and also represent the mid_market_rate relative to both smallest asset units.
+    //  Note that we should verify what the orderbook wants from us at some point; but either way we can decide how to represent it in here independent from the orderbook's representation.
+    let buy_amount = (sell_amount as f64 * rate).ceil() as u64;
 
     Order {
         sell_amount,
-        buy_amount: buy_amount.ceil() as u64,
+        buy_amount,
     }
 }
 
@@ -66,10 +86,12 @@ where
 mod tests {
     use super::*;
 
+    #[derive(Copy, Clone)]
     struct Book {
         locked_funds: u64,
     }
 
+    #[derive(Copy, Clone)]
     struct Wallet {
         balance: u64,
         fees: u64,
@@ -151,17 +173,23 @@ mod tests {
 
     #[test]
     fn given_a_rate_return_order_with_both_amounts() {
-        let wallet = Wallet::new(1051, 1);
 
+        let wallet = Wallet::new(1051, 1);
         let book = Book::new(50);
 
-        let order = new_order(wallet, book, 9999, 10.0, Spread::new(0.0).unwrap());
-        // 1 Buy => 10 Sell
-        // ? Buy => 1000 sell
-        // 100 Buy => 1000 Sell
+        let order = new_order(wallet, book, 9999, 0.1, Spread::new(0.0).unwrap());
 
+        // 1 Sell => 0.1 Buy
+        // 1000 Sell => 100 Buy
         assert_eq!(order.sell_amount, 1000);
-        assert_eq!(order.buy_amount, 100)
+        assert_eq!(order.buy_amount, 100);
+
+        let order = new_order(wallet, book, 9999, 10.0, Spread::new(0.0).unwrap());
+
+        // 1 Sell => 10 Buy
+        // 1000 Sell => 10000 Buy
+        assert_eq!(order.sell_amount, 1000);
+        assert_eq!(order.buy_amount, 10000);
     }
 
     #[test]
@@ -170,14 +198,29 @@ mod tests {
 
         let book = Book::new(50);
 
-        let order = new_order(wallet, book, 9999, 10.0, Spread::new(0.03).unwrap());
-        // 1 Buy => 10 Sell
-        // ? Buy => 1000 sell
-        // 100 Buy => 1000 Sell
-        // 3% spread
-        // 103 Buy => 1000 Sell
+        let order = new_order(wallet, book, 9999, 0.1, Spread::new(0.03).unwrap());
 
+        // 1 Sell => 0.1 Buy
+        // 3% spread
+        // 1000 Sell => 103 Buy
         assert_eq!(order.sell_amount, 1000);
+        // TODO: Why would it lose precision here? - I would not expect it to be 103 here (all results are within natural numbers)
         assert_eq!(order.buy_amount, 104); // Rounding up taking in account precision loss
     }
+
+    #[test]
+    fn given_a_rate_and_very_low_spread_return_order_that_is_still_profitable() {
+        let wallet = Wallet::new(1051, 1);
+
+        let book = Book::new(50);
+
+        let order = new_order(wallet, book, 9999, 0.1, Spread::new(0.0003).unwrap());
+
+        // 1 Sell => 0.1 Buy
+        // 0.003% spread
+        // 1000 Sell => 101 Buy
+        assert_eq!(order.sell_amount, 1000);
+        assert_eq!(order.buy_amount, 101); // Rounding up taking in account precision loss
+    }
+
 }
